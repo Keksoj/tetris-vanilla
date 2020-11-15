aa6095c4e52da5c38a114daf9c90e46fb8eee771
Emmanuel Bosquet <bjokac@gmail.com>
2020-11-15 16:50:48 +0100

    typing is good for you

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-import { sayhi } from "./tetromino.js";
+import { sayhi, Tetromino } from "./tetromino.js";
+
+/**
+ * @typedef {object} Tetromino
+ * @property {Number} spin
+ * @property {[{x: number, y: number}]} blocks
+ */
 
 var blockSize = 30;
 var timer;
@@ -9,3 +15,22 @@ canvas.height = 17 * blockSize;
 var ctx = canvas.getContext("2d");
 
 sayhi();
+
+/**
+ * The all-encompassing object to describe the game state
+ * @property {Number} score
+ * @property {Tetromino} [tetromino]
+ */
+const game = {
+    score: 0,
+    tetromino: null,
+};
+
+start();
+
+function start() {
+    game.score = 5;
+    game.tetromino = new Tetromino();
+    console.log(game);
+    game.tetromino;
+}
--- tetromino.js
+++ tetromino.js
@@ -1,5 +1,95 @@
-export { sayhi };
+function tickDown(tetromino) {
+    for (i = 0; i < 4; i++) {
+        tetromino.blocks[i].y -= 1;
+    }
+}
+function pushLeft(tetromino) {
+    for (i = 0; i < 4; i++) {
+        tetromino.blocks[i].x -= 1;
+    }
+}
+function pushRight(tetromino) {
+    for (i = 0; i < 4; i++) {
+        tetromino.blocks[i].x += 1;
+    }
+}
+
+/**
+ * A group a four blocks to move around
+ * @typedef {object} Tetromino
+ * @property {Number} spin
+ * @property {[{x: number, y: number}]} blocks
+ * 
+ */
+class Tetromino {
+    /**
+     * all possible coordinates for Tetrominoes
+     * @type {[{x: number, y: number}]} coordinates
+     */
+    blockCoordinates = [
+        // T
+        [
+            { x: 4, y: 17 },
+            { x: 5, y: 17 },
+            { x: 6, y: 17 },
+            { x: 5, y: 18 },
+        ],
+        // L
+        [
+            { x: 5, y: 19 },
+            { x: 5, y: 18 },
+            { x: 5, y: 17 },
+            { x: 6, y: 17 },
+        ],
+    ];
+
+    constructor() {
+        var type = Math.round(
+            Math.random() * (this.blockCoordinates.length - 1)
+        );
+
+        this.spin = 0;
+        this.blocks = this.blockCoordinates[type];
+    }
+
+    tickDown() {
+        for (i = 0; i < 4; i++) {
+            this.blocks[i].y -= 1;
+        }
+    }
+    pushLeft() {
+        for (i = 0; i < 4; i++) {
+            this.blocks[i].x -= 1;
+        }
+    }
+    pushRight() {
+        for (i = 0; i < 4; i++) {
+            this.blocks[i].x += 1;
+        }
+    }
+
+    draw(ctx, blockSize) {
+        ctx.save();
+        ctx.fillStyle = "#ff0000";
+        for (i = 0; i < 4; i++) {
+            ctx.fillRect(
+                this.blocks[i].x,
+                this.blocks[i].y,
+                blockSize,
+                blockSize
+            );
+        }
+        ctx.restore();
+    }
+}
+
+class truc extends Tetromino {
+    constructor() {
+        super("t");
+    }
+}
 
 const sayhi = () => {
     console.log("hi");
 };
+export { sayhi, Tetromino };
